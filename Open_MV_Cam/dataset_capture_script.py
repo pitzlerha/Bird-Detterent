d4d2f9b8ed462a78699b40efef75e3c7ac91738a
luukel <124745525+luukel@users.noreply.github.com>
2023-06-06 07:47:42 -0700

    Final OpenMV Camera Code

--- Open_MV_Cam/dataset_capture_script.py
+++ Open_MV_Cam/dataset_capture_script.py
@@ -12,7 +12,7 @@ sensor.set_auto_whitebal(False) # Turn off white balance.
 clock = time.clock() # Tracks FPS.
 extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.RGB565)
 
-birdsthreshold = 0.7
+birdsthreshold = 0.55
 led = pyb.LED(1)
 led.off()
 net = None
@@ -25,16 +25,20 @@ sensor.skip_frames(time = 2000) # Give the user time to get ready.
 extra_fb.replace(sensor.snapshot())
 print("Saved background image!")
 
-text = "1\n"
-text2= "0\n"
-'''
-communication method (need to change)
+text = "1" #bird
+text2= "0" #no bird
+text3 = "2" #obstruct
+
+
+#communication method (need to change)
 data = ustruct.pack("<%ds" % len(text), text)
 data2 = ustruct.pack("<%ds" % len(text2), text2)
+data3 = ustruct.pack("<%ds" % len(text3), text3)
 bus = pyb.I2C(2, pyb.I2C.SLAVE, addr=0x12)
 bus.deinit() # Fully reset I2C device...
 bus = pyb.I2C(2, pyb.I2C.SLAVE, addr=0x12)
-'''
+print(data, " DATA BITS")
+#print(data2, " DATA")
 
 try:
     # load the model, alloc the model file on the heap if we have at least 64K free after loading
@@ -48,6 +52,7 @@ try:
 except Exception as e:
     raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
 
+clock = time.clock()
 
 
 while(True):
@@ -58,17 +63,7 @@ while(True):
 
 
     # default settings just do one detection... change them to search the image...
-    '''
-    for obj in net.classify(img, min_scale=1.0, scale_mul=0.8, x_overlap=0.5, y_overlap=0.5):
-        print("**********\nPredictions at [x=%d,y=%d,w=%d,h=%d]" % obj.rect())
-        img.draw_rectangle(obj.rect())
-        # This combines the labels and confidence values into a list of tuples
-        predictions_list = list(zip(labels, obj.output()))
 
-        for i in range(len(predictions_list)):
-            print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
-    '''
-    # default settings just do one detection... change them to search the image...
     for obj in net.classify(img, min_scale=1.0, scale_mul=0.8, x_overlap=0.5, y_overlap=0.5):
         print("**********\nPredictions at [x=%d,y=%d,w=%d,h=%d]" % obj.rect())
         img.draw_rectangle(obj.rect())
@@ -79,15 +74,28 @@ while(True):
             print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
 
     print(clock.fps(), "fps")
-    print(clock.fps(), "fps")
+
     idx = labels.index('bird')
-'''
+    idx2 = labels.index('obstruct')
+
     if ((obj.output()[idx] > birdsthreshold) and (sim.min() < MIN_TRIGGER_THRESHOLD)):
         try:
             bus.send(ustruct.pack("<h", len(data)), timeout=100) # Send the len first (16-bits).
             try:
                 bus.send(data, timeout=100) # Send the data second.
             except OSError as err:
+                 pass # Don't care about errors - s+-o pass.
+        # Note that there are 3 possible errors. A timeout error, a general purpose error, or
+        # a busy error. The error codes are 116, 5, 16 respectively for "err.arg[0]".
+        except OSError as err:
+            pass
+
+    elif (obj.output()[idx2] > birdsthreshold  ):
+        try:
+             bus.send(ustruct.pack("<h", len(data3)), timeout=100) # Send the len first (16-bits).
+             try:
+                bus.send(data3, timeout=100) # Send the data second.
+             except OSError as err:
                  pass # Don't care about errors - s+-o pass.
         # Note that there are 3 possible errors. A timeout error, a general purpose error, or
         # a busy error. The error codes are 116, 5, 16 respectively for "err.arg[0]".
@@ -106,4 +114,9 @@ while(True):
         # a busy error. The error codes are 116, 5, 16 respectively for "err.arg[0]".
             except OSError as err:
                 pass
-'''
+
+
+
+
+
+
